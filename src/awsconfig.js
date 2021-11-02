6a5cc62fd08087d51cacf93763a6c11cdd2aaa37
BugofBook <BugofBook>
2021-11-02 23:12:58 +0800

    add config
    aws-amplify config

---
+++ src/awsconfig.js
@@ -0,0 +1,15 @@
+const awsmobile = {
+    Auth: {
+        // 服務所在的區域
+        region: 'ap-northeast-1',
+        // 使用者集區 -> 使用者ID
+        userPoolId: 'ap-northeast-1_bY2byhlCa',
+        // 使用者集區 -> 應用程式用戶端ID
+        userPoolWebClientId: '21u50hboia4s5q0sbk6pbdfmss',
+        // 身份集區 -> 身份集區ID
+        identityPoolId: 'ap-northeast-1:89fa296a-cddb-4937-b82e-2d8dc9732592',
+        mandatorySignIn: false,
+    },
+}
+
+export default awsmobile
